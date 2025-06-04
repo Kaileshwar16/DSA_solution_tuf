9a513e2ba3e92886b581bd48d6cc1ba7aa73f7f7
Kaileshwar16 <kaileshwar2005@gmail.com>
2025-06-04 20:48:31 +0530

    _

--- array/leader.cpp
+++ array/leader.cpp
@@ -2,12 +2,14 @@
 #include<algorithm>
 using namespace std;
 
+
 int main(){
   int arr[] ={12,6,7,3,1,4,2};
   int n=sizeof(arr)/sizeof(arr[0]);
   int ans[n];
   int maxe=-10000;
   int j=0;
+
   for(int i=n-1; i>=0; i--){
     if(arr[i]>maxe){
       ans[j]=arr[i];
